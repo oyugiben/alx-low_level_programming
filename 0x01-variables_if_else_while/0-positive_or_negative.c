1d9a7463150f807dad69148be86b8bb883c1bda7
oyugiben <sirbensonoyugi@gmail.com>
2022-03-12 01:57:02 +0300

    positivenegative2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive", n);
+		printf("%d is positive", n);
 	else if (n == 0)
-		printf("%i is zero", n);
+		printf("%d is zero", n);
 	else
-		printf("%i is negative", n);
+		printf("%d is negative", n);
 	return (0);
 }

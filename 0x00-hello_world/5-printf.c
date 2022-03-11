e5e33443810fe6a12d8895abd3eaf07b5c38edc9
oyugiben <sirbensonoyugi@gmail.com>
2022-03-11 17:35:16 +0300

    completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", s);
+	return (0);
+}

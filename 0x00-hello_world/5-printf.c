aa85d29a33e112ac205ff474dcd20c39a9e82b91
oyugiben <sirbensonoyugi@gmail.com>
2022-03-11 17:57:46 +0300

    correcte printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char *s = "with proper grammar, but the outcome is a piece of art, ";
+	char *s = "with proper grammar, but the outcome is a piece of art,\n";
 
 	printf("%s", s);
 	return (0);

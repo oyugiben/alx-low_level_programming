0f56a8e55da82056cac580c34c45fffb749a72b3
oyugiben <sirbensonoyugi@gmail.com>
2022-03-15 18:09:27 +0300

    changes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+	int x = 0;
+
+	while (x = 0; x <= 10; x++)
+	{
+		while (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}

c2b24925bcdc3972b302e7a73e116b959beb63f6
oyugiben <sirbensonoyugi@gmail.com>
2022-03-15 17:05:21 +0300

    trying

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,13 @@
  */
 int main(void)
 {
-	char x = '_putchar';
-
-	_putchar(x);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	return (0);
 }

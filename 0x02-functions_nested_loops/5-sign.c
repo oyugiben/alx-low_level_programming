f64ddc36ef1e679daae1808eceefc2e75c36a65e
oyugiben <sirbensonoyugi@gmail.com>
2022-03-15 19:59:21 +0300

    several

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign will print out to sts output the sign of an interger
+ * @n: the interger to be checked
+ *
+ * Return: 1 if positive, 0 if 0 and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute value of input integer
+ *
+ * Return: absolute value of integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (0 - i);
+}

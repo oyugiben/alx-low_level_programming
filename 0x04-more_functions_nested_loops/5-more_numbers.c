f6daf510e4d96b14e1eea5069f2cccdcdbe136dc
oyugiben <sirbensonyugi@gmail.com>
2022-03-18 03:58:39 +0300

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints
+ *
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; 1++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}

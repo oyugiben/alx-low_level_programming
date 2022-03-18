9403b1b43f45da1438a0323e1b321bc8db0ae131
oyugiben <sirbensonyugi@gmail.com>
2022-03-18 04:52:17 +0300

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line on the terminal
+ * @n: number of slashes to be printed
+ *
+ * Return: always 0
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = n;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n >= 0)
+		{
+			i = j - n;
+			while (i > 0)
+			{
+				_putchar(' ');
+				i--;
+			}
+			putchar('\\');
+			putchar('\n');
+			n--;
+		}
+	}
+}

d05cbc985338c17150bbdfc4c7b1d250ce60b59d
oyugiben <sirbensonyugi@gmail.com>
2022-03-18 03:44:58 +0300

    print_most_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_numbers - prints numbers 0 to 9
  *
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers 0 to 9 ignoring 2 and 4
+ *
+ * Return: always 0;
+ */
+void print_most_numbers(void);
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	return (0);
+}

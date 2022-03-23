f7dba3a41350d7ee3af9405efb8bbb9adb8fbcef
oyugiben <sirbensonoyugi@gmail.com>
2022-03-23 19:14:18 +0300

    printing the numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
  * print_numbers - prints numbers 0 to 9
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }

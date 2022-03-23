aaa793f8283d97b471523178025e66b7d5acc392
oyugiben <sirbensonoyugi@gmail.com>
2022-03-23 20:46:54 +0300

    number 5 numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,24 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
+	int l;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 13; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j < 10)
+			{
+				k = j;
+			}
+			else
+			{
+				k = j % 10;
+				l = j / 10;
+				_putchar(l + '0');
+			}
+			_putchar(k + '0');
 		}
 		_putchar('\n');
 	}

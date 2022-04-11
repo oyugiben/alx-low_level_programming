f9c516959851623d1cdf381a01006a989750f0e0
oyugiben <sirbensonoyugi@gmail.com>
2022-04-11 17:16:09 +0300

    fixed all double inclusion cases and struct descriptions for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_STRUCT
 #define DOG_STRUCT
 
+/**
+ * struct dog - Builds a dog type
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ *
+ * Used to buld a dog type
+ */
 struct dog
 {
 	char *name;
